80d5c42d704e6b380ed7ac5ab616dba4d944be58
Jason G. Johnson <jasongjohnson@gmail.com>
2015-10-09 09:49:49 -0700

    init

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,4 +13,14 @@
 class jasontest {
 
 # test 2
+
+ dsc::lcm_config {'disable_lcm':
+  refresh_mode => 'Disabled',
+}->
+
+dsc_windowsfeature {'IIS':
+    dsc_ensure => 'present',
+    dsc_name   => 'Web-Server',
+  } 
+
 }
