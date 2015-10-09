39e0eb3b11c07e754f97f87e4cb1436e4ff9c864
Jason G. Johnson <jasongjohnson@gmail.com>
2015-10-09 10:10:00 -0700

    a

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,18 +10,28 @@
 #
 # Sample Usage:
 #
-class jasontest {
-  # test 2
-
-  $websitename = "thisiscrazy"
-  $destinationpath = 'C:\inetpub\FourthCoffee'
-
+class jasontest (
+  $websitename        = "thisiscrazy",
+  $destinationpath    = 'C:\inetpub\FourthCoffee',
+  $defaultwebsitepath = 'C:\inetpub\wwwroot',) {
+ 
   dsc::lcm_config { 'disable_lcm': refresh_mode => 'Disabled', } ->
   dsc_windowsfeature { 'IIS':
     dsc_ensure => 'present',
     dsc_name   => 'Web-Server',
   }
-
+  # Install the ASP .NET 4.5 role
+  dsc_windowsfeature { 'AspNet45':
+    dsc_ensure => 'present',
+    dsc_name   => 'Web-Asp-Net45',
+  } ->
+  # Stop an existing website (set up in Sample_xWebsite_Default)
+  dsc_xwebsite { 'Stop DefaultSite':
+    dsc_ensure       => 'present',
+    dsc_name         => 'Default Web Site',
+    dsc_state        => 'Stopped',
+    dsc_physicalpath => $defaultwebsitepath,
+  } ->
   dsc_xwebsite { $websitename:
     dsc_ensure       => 'present',
     dsc_name         => $websitename,
